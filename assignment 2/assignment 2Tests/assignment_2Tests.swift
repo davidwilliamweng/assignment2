2223f5884c882b4df0058b2f5393f78693d9288d
davidwilliamweng <38621204+davidwilliamweng@users.noreply.github.com>
2018-04-29 18:19:16 +1000

    Add files via upload

---
+++ assignment 2/assignment 2Tests/assignment_2Tests.swift
@@ -0,0 +1,36 @@
+//
+//  assignment_2Tests.swift
+//  assignment 2Tests
+//
+//  Created by Qin Mao on 2018/4/29.
+//  Copyright © 2018年 david. All rights reserved.
+//
+
+import XCTest
+@testable import assignment_2
+
+class assignment_2Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
